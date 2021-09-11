7cfcbf85cd440a0c943a8af76514f646d6b2bdca
Andrei-Mihai Nicolae <me@nclandrei.com>
2021-09-11 17:02:02 +0300

    Add initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: `Andrei's Blog`,
+    name: `Andrei Nicolae`,
+    siteUrl: `https://anicolae.ro`,
+    description: `Andrei's software development blog.`,
+
+    hero: {
+      heading: `Perspectives on technology, design and business from the team at Narative.`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/amnicolae`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/nclandrei`,
+      },
+    ],
+  },
+  plugins: ['@narative/gatsby-theme-novela'],
+};
